2a6474c2099b400dc8d9b07de806a87cd5a3b6ea
soomin-jin <dyd002700@gmail.com>
2025-06-23 15:15:57 +0900

    [ing]등록페이지

---
+++ src/app/admin/gift/create/page.tsx
@@ -0,0 +1,9 @@
+const CreateGiftPage = () => {
+  return (
+    <div className="flex h-full flex-1 flex-col justify-center gap-[10px] px-[10px]">
+      등록페이지
+    </div>
+  );
+};
+
+export default CreateGiftPage;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,7 +8,8 @@ const AdminPage = () => {
   return (
     <div className="flex h-full flex-1 flex-col justify-center gap-[10px] px-[10px]">
       <Button onClick={() => router.push('/admin/user')} label="유저 등록 및 관리" />
-      <Button onClick={() => router.push('/admin/gift')} label="선물 관리" />
+      <Button onClick={() => router.push('/admin/gift/create')} label="선물 등록" />
+      <Button onClick={() => router.push('/admin/gift')} label="선물 수정" />
     </div>
   );
 };
